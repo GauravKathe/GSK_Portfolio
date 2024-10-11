6a05b3c9dd0c4696d55f671b2ccfeb19553d27bd
Gaurav <gauravkatheoff@gmail.com>
2024-10-11 22:17:02 +0530

    hero.jsx updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,15 @@ const Hero = () => {
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
                     {/*<h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Gaurav Kathe</h1>*/}
-                    <h1 className="pb-16 text-6xl font-thin tracking-tight bg-gradient-to-r from-pink-400 
-                    via-slate-600 to-indigo-500 bg-clip-text text-transparent lg:mt-16 lg:text-7xl">Gaurav Kathe</h1>
+                    <h1 className="pb-0 mb-2 text-6xl font-thin tracking-tight bg-gradient-to-r from-pink-400 
+                    via-slate-600 to-indigo-500 bg-clip-text text-transparent lg:mt-16 lg:text-7xl">Hi There..!</h1>
+                    <h1 className="mt-2 text-4xl font-thin tracking-tight bg-gradient-to-r from-pink-400 
+                    via-slate-600 to-indigo-500 bg-clip-text text-transparent lg:text-5xl">I'm Gaurav Kathe</h1>
+                    <h4 className="mt-1">java.Error:name-replacement &lt;People call me Garry&gt;</h4>
 
-                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
-                    bg-clip-text text-4xl tracking-tight text-transparent" > Full Stack Developer </span>
+                    <span className="mt-[1em] bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+                    bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer
+                    </span>
                     
                     <p className="my-2  max-w-xl py-6 font-light tracking-tighter text-justify">{HERO_CONTENT}</p>
                 </div>
