f8c4cd3b10abb312d3684d2d6b4da3648b77a89b
Gaurav <gauravkatheoff@gmail.com>
2024-10-14 23:15:56 +0530

    technologies field added in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,35 @@ import { useEffect } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Technologie from "./components/Technologie";
 
 const App = () => {
   useEffect(() => {
-    // Set embedded chatbot config
+    // Set the chatbot configuration
     window.embeddedChatbotConfig = {
       chatbotId: "JnqlXrUnlSBJY_ngfMW-8",
       domain: "www.chatbase.co",
     };
 
-    // Load the chatbot script
+    // Load the chatbot script dynamically
     const script = document.createElement("script");
     script.src = "https://www.chatbase.co/embed.min.js";
     script.setAttribute("chatbotId", "JnqlXrUnlSBJY_ngfMW-8");
     script.setAttribute("domain", "www.chatbase.co");
     script.defer = true;
 
+    // Append the script to the document body
     document.body.appendChild(script);
-  }, []); // Run once on component mount
+  }, []); // Empty dependency array to run the effect only once
 
   return (
     <div className="relative min-h-screen">
       <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-        {/* New Background Gradient */}
+        {/* Background Gradient */}
         <div className="fixed top-0 -z-10 h-full w-full">
           <div
-            className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 
-            bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+            className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950
+              bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
           ></div>
         </div>
 
@@ -37,6 +39,7 @@ const App = () => {
           <Navbar />
           <Hero />
           <About />
+          <Technologie/>
         </div>
       </div>
     </div>
---
+++ src/components/Technologie.jsx
@@ -0,0 +1,11 @@
+const Technologie = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">
+        Techno<span className="text-neutral-500">Logies</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Technologie
